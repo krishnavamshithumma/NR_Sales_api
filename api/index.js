15fe446764ba104cebd829e6c14fb715236ffa19
Krishnavamshiwk <krishnavamshi.thumma@wishkarma.com>
2025-03-15 11:48:59 +0530

    Fixed Vercel API routing

--- api/index.js
+++ api/index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use("/api", analysisRoutes);
 
 // Start server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// for vercel
+module.exports = app;
